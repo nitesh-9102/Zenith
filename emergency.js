c2a4ac2e0e62ce8659b9fc28427334f27d93aa0b
Nitesh Gunupudi <niteshgunu@gmail.com>
2024-03-24 18:42:02 +0530

    nav update

--- emergency.js
+++ emergency.js
@@ -4,18 +4,17 @@ const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const mess = document.getElementById("message");
 const subject = document.getElementById("subject");
+const bodyMessage = `Name: ${nameElement.value}<br> Email: ${emailElement.value}<br> Phone: ${phoneElement.value}<br> Subject: ${subjectElement.value}`;
+Email.send({
+  Host: "smtp.elasticemail.com",
+  Username: "username",
+  Password: "password",
+  To: "them@website.com",
+  From: "you@isp.com",
+  Subject: "This is the subject",
+  Body: bodyMessage,
+}).then((message) => alert(message));
 
-function sendEmail() {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "niteshgunu@gmail.com",
-    Password: "75E3C25F90587699CF116E2B48A5733329DE",
-    To: "niteshgunu@gmail.com",
-    From: "niteshgunu@gmail.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   sendEmail();
